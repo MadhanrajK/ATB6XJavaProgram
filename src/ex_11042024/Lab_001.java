2280ff9722525276e0c723d2aaa2e9243fe5e95a
MadhanrajK <mr.madhanraj.k@gmail.com>
2024-05-07 22:06:32 +0530

    Initial commit

---
+++ src/ex_11042024/Lab_001.java
@@ -0,0 +1,8 @@
+package ex_11042024;
+
+public class Lab_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Madhan!");
+    }
+}
+
---
+++ src/ex_11042024/Lab_002.java
@@ -0,0 +1,7 @@
+package ex_11042024;
+/*
+* Author - Madhan
+* Program - Lab No 2
+ */
+public class Lab_002 {
+}
